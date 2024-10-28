bdabe0f718c6dce214aa0182d5fd10a89460864d
Mohamad Feby <mhmdfeby6@gmail.com>
2024-10-29 00:18:43 +0700

    Portofolio Mhmd Feby

---
+++ dist/js/script.js
@@ -0,0 +1,36 @@
+//humburger
+const toggle1 = document.querySelector('#toggle1');
+const navMenu = document.querySelector('#nav-menu');
+
+toggle1.addEventListener('click', function(){
+  toggle1.classList.toggle('toggle1-active');
+  navMenu.classList.toggle('hidden');
+
+});
+
+//navbar
+
+
+//darkmode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function() {
+  if (darkToggle.checked) {
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+
+});
+
+//togglemove
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
+  
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+
+  },
+  plugins: [],
+}
+
